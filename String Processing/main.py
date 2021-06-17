ac07dd131339a915c58483749a152bf070d38409
fatih <fatiharslanedu@gmail.com>
2021-06-17 10:42:16 +0300

    palindrome

--- String Processing/main.py
+++ String Processing/main.py
@@ -1,13 +1,15 @@
 '''
 111221 is read of as "three 1s, two 2s, then one 1" or 312211
 '''
+
+
 def next_number(s):
     result = []
     i = 0
     count = 1
     s = [int(x) for x in s]
     print(s)
-    while i < len(s): #todo: better than edu. O(n)
+    while i < len(s):  # todo: better than edu. O(n)
         if i == len(s) - 1:
             result.append(count)
             result.append(s[i])
@@ -16,10 +18,11 @@ def next_number(s):
         elif s[i] != s[i + 1]:
             result.append(count)
             result.append(s[i])
-            count = 1 
-        i += 1   
+            count = 1
+        i += 1
     result = [str(x) for x in result]
     return ''.join(result)
+
 
 def spreadsheet_encode_column(col_str):
     upper = len(col_str) - 1
@@ -28,12 +31,35 @@ def spreadsheet_encode_column(col_str):
         num += 26**upper * (ord(i) - ord('A') + 1)
         upper -= 1
     return num
-    
+
+def is_palindrome(s: str):
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        while not s[i].isalnum() and i < j:
+            i += 1
+        while not s[j].isalnum() and i < j:
+            j -= 1
+        
+        if s[i].lower() != s[j].lower():
+            return False
+        i += 1
+        j -= 1
+    return True
+
 def main():
     # todo: 1
     # print(next_number('13112221'))
     # todo: 2
     print(spreadsheet_encode_column("ZZ"))
+    # todo 3
+    s = "was it a cat I saw?"
+    s = ''.join([i for i in s if i.isalnum()]).replace(' ', '').lower()
+    print(s == s[::-1])
+    s = "was it a cat I saw?"
+    print(is_palindrome(s))
+    
+
 
 if __name__ == "__main__":
     main()
--- String Processing/script.py
+++ String Processing/script.py
@@ -18,8 +18,23 @@ def spreadsheet_encode_column(col_str):
         num += 26**upper * (ord(i) - ord('A') + 1)
         upper -= upper
     return num
-                
-    
+
+def is_palindrome(s: str):
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        while not s[i].isalnum() and i < j:
+            i += 1
+        while not s[j].isalnum() and i < j:
+            j -= 1
+        
+        if s[i].lower() != s[j].lower():
+            return False
+        
+        i += 1
+        j -= 1
+    return True
+            
 def main():
     # todo 1
     '''
@@ -31,10 +46,18 @@ def main():
         print(s)
     '''
     # todo 2
+    '''
     print(ord("A") - ord("A") + 1)
     print(ord("Z") - ord("A") + 1)
     print(spreadsheet_encode_column("ZZ"))
+    '''
+    # todo 3
+    s = "was it a cat I saw?"
+    s = ''.join([i for i in s if i.isalnum()]).replace(' ', ' ').lower()
+    print(s == s[::-1])    
+    s = "Was it a cat I saw?"
+    print(is_palindrome(s))
     
-
+    
 if __name__ == "__main__":
     main()
